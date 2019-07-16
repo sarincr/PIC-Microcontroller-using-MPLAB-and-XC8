2399d47c9029849d5e1d7dfef5f548767fde24a0
Sarin CR <38061354+sarincr@users.noreply.github.com>
2019-07-16 07:45:54 -0700

    GPIO_Toggle

---
+++ 06.GPIO_Toggle.X/main.c
@@ -0,0 +1,16 @@
+#include <xc.h>                                        //PIC hardware mapping
+#define _XTAL_FREQ 500000                               //Used by the XC8 delay_ms(x) macro
+
+void main(void) 
+{
+    unsigned int delay;                                 //create a variable of 16 bits in length
+    
+    TRISC = 0;                                          //using pin as output
+    delay = 11250;
+    while (1) 
+    {       
+        while(delay-- != 0)continue;                    //each instruction is 8us (1/(500KHz/4))
+        PORTCbits.RC1 ^= PORTCbits.RC1;                            //toggle the LED
+        delay = 11250;                                  //assign a value since it is at 0 from the delay loop
+    }
+}
---
+++ 06.GPIO_Toggle.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=06.GPIO_Toggle.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/06.GPIO_Toggle.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=06.gpiotoggle.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/06.gpiotoggle.x.tar
